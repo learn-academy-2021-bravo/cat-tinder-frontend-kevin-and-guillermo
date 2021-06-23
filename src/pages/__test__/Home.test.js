fad64d5b6fb6c3ca341af611c2663e996c55c7ce
guillermogb88 <guillermo.garcia08@yahoo.com>
2021-06-23 13:13:01 -0700

    4 test complete on header

--- src/pages/__test__/Home.test.js
+++ src/pages/__test__/Home.test.js
@@ -4,3 +4,13 @@ import Adapter from 'enzyme-adapter-react-16'
 import Home from '../Home'
 
 Enzyme.configure({ adapter: new Adapter()})
+
+describe('When App renders', () => {
+    it('provides a route "/" to the home component', () => {
+        const renderedApp = shallow(<Home/>)
+        const renderedHomeRoute = renderedApp.find("h1")
+        expect(renderedHomeRoute.text()).toEqual("this is my home page")
+
+       
+    })
+})
